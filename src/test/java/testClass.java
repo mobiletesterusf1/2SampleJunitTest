a4cf405763febefdc58351e1312e1d54f9cb8d62
Senthil Kumar R <Senthil Kumar R>
2017-02-28 01:52:05 -0500

    2222222

--- src/test/java/testClass.java
+++ src/test/java/testClass.java
@@ -1,4 +1,10 @@
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.server.FirefoxDriverProvider;
 
 
 
@@ -9,6 +15,14 @@ public class testClass {
 	public void method(){
 		
 		System.out.println("Hello");
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\senth\\Downloads\\chromeDriver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20l, TimeUnit.SECONDS);
+		driver.get("http://tamil.oneindia.com/");
+		
+		System.out.println(driver.getTitle());
 	}
 
 }
